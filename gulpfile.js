044592fecd3610248630a468e27353013174285f
walshyb <bmwalshy@gmail.com>
2017-01-15 17:06:21 -0500

    Update gulpfile to watch for sass & js changes

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,35 @@ var babelify = require('babelify');
 var browserify = require('browserify');
 var buffer = require('vinyl-buffer');
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 var source = require('vinyl-source-stream');
 var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
 
-gulp.task('default', function () {
-  process.env.NODE_ENV = 'production';
+gulp.task('sass', function() {
+  return gulp.src('./assets/stylesheets/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./public/stylesheets/'));
+});
 
+gulp.task('apply-production', function() {
+  process.env.NODE_ENV = 'production';
+});
+
+gulp.task('default', function() {
+  gulp.watch('./assets/stylesheets/**/*.scss', ['sass']);
+  gulp.watch('./assets/javascripts/**/*.js', ['javascripts']);
+});
+
+gulp.task('javascripts', function() {
+  console.log('in js');
   var bundler = browserify({
     entries: 'assets/javascripts/index.js',
   });
 
-  bundler.transform(babelify);
+  bundler.transform(babelify, {presets: ["es2015", "react"]});
 
-  bundler.bundle()
+  return bundler.bundle()
     .on('error', function (err) { console.error(err); })
     .pipe(source('app.js'))
     .pipe(buffer())
@@ -23,4 +38,5 @@ gulp.task('default', function () {
     .pipe(uglify())
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('public'));
+
 });
