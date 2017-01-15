f1c1ed015e53b50ba6d8340a60da75b24a70c128
walshyb <bmwalshy@gmail.com>
2017-01-15 17:58:05 -0500

    display current date

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -2,4 +2,33 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import List from './components/list'
 
-ReactDOM.render(<div><List /> <h1>Hello, World! </h1></div>, document.getElementById('app'));
+export default class TaskManager extends Component {
+
+  currentDate() {
+    var date = new Date();
+    const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var months = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
+   
+    var dayOfWeek = weekdays[date.getDay()];
+    var month = months[date.getUTCMonth()]; //months from 1-12
+    var day = date.getUTCDate();
+    var year = date.getUTCFullYear();
+
+    return <h1>{dayOfWeek}, {month} {day}, {year} </h1>; 
+  };
+
+  render() {
+    return (
+      <div>
+        { this.currentDate() }
+        <List /> 
+        <h1> Hello world! </h1>
+      </div> 
+    );
+  
+  };
+}
+
+ReactDOM.render(React.createElement(TaskManager) , document.getElementById('app'));
