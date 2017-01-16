11fb7d4d6c78b5b088ad3083b362f1c54a292a22
walshyb <bmwalshy@gmail.com>
2017-01-15 22:31:06 -0500

    get categories from chrome storage

--- assets/javascripts/components/SelectCategory.js
+++ assets/javascripts/components/SelectCategory.js
@@ -16,12 +16,25 @@ export default class SelectCategory extends Component {
     this.setState({value: event.target.value});
   };
 
+  componentDidMount() {
+    
+    chrome.storage.sync.get(null, function(items) {   // get all keys from chrome storage
+      var allKeys = Object.keys(items);               // store them in list
+      var list = allKeys.map((key) =>                 // for each key, create JSX object
+        <option value={key}> {key} </option>
+      );
+
+      this.setState( {categories: list} );            // assign list of JSX objects to attribute 'categories'
+    }.bind(this));                                    // bind 'this' so we can have access to setState in chrome method
+  };
+
   render() {
     return (
       <form>
         <label>
           <select value={this.state.value} onChange={this.handleChange}>
             <option value=''>Category</option> 
+            { this.state.categories } 
             <option value='add-category'>+ Create New</option>
           </select>
         </label>
