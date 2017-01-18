8f63f101cc37c151dddb0a0db70c70cb9d148007
walshyb <bmwalshy@gmail.com>
2017-01-18 10:15:35 -0500

    can successfully remove tasks

--- assets/javascripts/components/CategoryListItem.js
+++ assets/javascripts/components/CategoryListItem.js
@@ -2,9 +2,43 @@ import React, { Component } from 'react';
 import Task from './Task';
 
 export default class CategoryListItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tasks: []
+    };
+    this.removeTask = this.removeTask.bind(this);
+  };
+
+  removeTask(task) {
+    console.log(this.state);
+    var index = this.state.tasks.indexOf(task);
+
+    var newArray = this.props.tasks;
+
+    if (index > -1 ) {
+      newArray.splice(index, 1);
+      this.setState({
+        tasks: newArray
+      });
+
+      chrome.storage.sync.set({ [this.props.categoryName] : newArray  });
+    }
+
+    
+  };
+
+  componentWillMount() {
+    this.setState({
+      tasks: this.props.tasks
+    });
+    
+  };
+
   render() {
     const tasks = this.props.tasks.map(function(task, i) {
-      return <Task key={`${this.props.categoryName}${i}`} text={task}/>;
+      return <Task key={`${this.props.categoryName}${i}`} text={task} taskId={i} removeTask={this.removeTask}/>;
     }.bind(this));
     return (
       <article className='category' id={this.props.categoryName}>
@@ -14,3 +48,5 @@ export default class CategoryListItem extends Component {
     );
   };  
 }
+
+
--- assets/javascripts/components/Task.js
+++ assets/javascripts/components/Task.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 
 export default class Task extends Component {
+  constructor(props){
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+  };
+  
+  handleChange() {
+    this.props.removeTask(this.props.text);
+  };
+
   render() {
     return (
       <li>
-        <input type='checkbox' />
-        {this.props.text}
+        <input type='checkbox' onChange={this.handleChange}/>
+        <span>{this.props.text}</span>
       </li>
     );
   };
