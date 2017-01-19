0fc52f9f2ff4fb93e9d73a39f8d7281584cd4768
walshyb <bmwalshy@gmail.com>
2017-01-19 12:13:04 -0500

    applymiddleware to root js file

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 import TaskManager from './components/TaskManager';
 import reducers from './redux/reducers';
 
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 
-const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+//const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider store={ createStoreWithMiddleware(reducers) }>
     <TaskManager />
   </Provider>
   , document.getElementById('app'));
