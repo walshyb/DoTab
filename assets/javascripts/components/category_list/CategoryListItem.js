1d6b3d744d7eca3e40fcad4038f63cb16a66ea40
walshyb <bmwalshy@gmail.com>
2017-05-25 19:59:16 -0400

    display edit options on edit-category click

--- assets/javascripts/components/category_list/CategoryListItem.js
+++ assets/javascripts/components/category_list/CategoryListItem.js
@@ -4,6 +4,18 @@ import Task from './Task';
 export default class CategoryListItem extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      editMode: false
+    };
+  };
+
+  handleClick = (event) => {
+    if(this.state.editMode) {
+      this.setState({editMode: false});
+    } else {
+      this.setState({editMode: true});
+    }
   };
 
   render() {
@@ -18,10 +30,20 @@ export default class CategoryListItem extends Component {
 
     return (
       <article className='category' id={this.props.id}>
-        <h2> 
-          {this.props.categoryName} 
-          <button className='edit-category'>✏️  </button>
-        </h2>
+        { !this.state.editMode ? (
+          <div className='view-mode'>
+            <h2> 
+              {this.props.categoryName} 
+              {/*TODO: make edit-category button be an svg of a pencil and not an emoji*/}
+              <button onClick={this.handleClick} className='edit-category'>✏️  </button>
+            </h2>
+          </div>
+        ) : (
+          <div className='edit-mode'>
+            <input className='edit-category-name' value={this.props.categoryName}/>
+            <button className='remove-category'>X</button> 
+          </div>
+        )}
         <ul className='tasks'> { tasks } </ul>
       </article>
     );
