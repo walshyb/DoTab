d84b41d6482287c18630b10ccf29cf9ed5c84f19
walshyb <bmwalshy@gmail.com>
2017-05-25 01:31:04 -0400

    style AddTaskBar input field/button and tasks list

--- assets/javascripts/components/category_list/Task.js
+++ assets/javascripts/components/category_list/Task.js
@@ -14,7 +14,7 @@ export default class Task extends Component {
       <li className='task'>
         <label>
           <input type='checkbox' onChange={this.handleChange} id={this.props.taskId}/>
-          <span>{this.props.text}</span>
+          <span className='task-text'>{this.props.text}</span>
         </label>
       </li>
     );
