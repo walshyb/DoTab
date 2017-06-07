81ee5edbfc8ee065ed8c673eafbf490c7eaba947
walshyb <bmwalshy@gmail.com>
2017-06-06 23:19:42 -0400

    create example state tree for ListManager

--- __test__/ListManager.spec.js
+++ __test__/ListManager.spec.js
@@ -7,30 +7,30 @@ import { Provider } from 'react-redux';
 import ConnectedListManager, { ListManager } from '../assets/javascripts/components/list_manager/ListManager';
 
 
-
-describe('>>>H O M E --- REACT-REDUX (Shallow + passing the {store} directly)', () =>{
+describe('List Manager Test', () =>{
   const initialState = {
     list_manager: {
-      category1: {
-        active: {
-          'ebeb': 'Task 1',
-          '2f9a': 'Task 2'
-        },
-        completed: {
+      categories: {
+        category1: {
+          active: {
+            'ebeb': 'Task 1',
+            '2f9a': 'Task 2'
+          },
+          completed: {}
         }
       }
     }
   }
-  const mockStore = configureStore()
+  const mockStore = configureStore();
   let store, container;
 
-  beforeEach(()=>{
+  beforeEach( () => {
     store = mockStore(initialState)
     container = shallow(<ConnectedListManager store={store} /> )  
-  })
+  });
 
   it('renders correctly', () => {
-    const tree = renderer.create(<ListManager />).toJSON();
+    const tree = renderer.create(<ConnectedListManager store={store} />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
